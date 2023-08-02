007343cd30158b7e6c033570ecd01e802e7c4747
majingze0508 <majingze0508@gmail.com>
2023-08-01 20:08:20 -0400

    Initial Entry

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/size';
+
+export const FocusHistory = ({ history }) => {
+  if (!history || history.length === 0) return <Text style={styles.title}>We haven't focused on anything yet</Text>;
+  
+  const renderItem = ({item}) => <Text style={styles.item}>- {item}</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on:</Text>
+      <FlatList
+        data={history}
+        renderItem={renderItem}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1
+  },
+  item: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    paddingTop: spacing.sm
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold'
+  }
+})
+
